6875a99822e763dd6e897fe3142c0660400b8f5f
MOM0BAMI <janendrenachari@gmail.com>
2026-04-23 13:56:02 +0200

    Fixes to js

--- js/catalog.js
+++ js/catalog.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   console.log("DOM ready, attempting to load JSON");
 
   // Use absolute path
-  $.getJSON("/data/lighting.json")
+$.getJSON("/fanucci-website/data/lighting.json")
     .done(function(data) {
       console.log("SUCCESS: JSON loaded, number of products:", data.length);
       products = data;
